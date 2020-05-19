c105069d6f05c925b20935c7760e333c13ed91a3
Grzegorz Andruszkiewicz <gandruszkiewicz@gmail.com>
2020-05-19 16:20:52 +0200

    login component created App.js mock modifications depends is user auth or not

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LayoutComponent from './components/Layout/LayoutComponent'
+import LoginComponent from './components/LoginComponent'
 import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <LayoutComponent/>
-    </div>
-  );
+class App extends Component {
+  
+  // needs to pass user when component is mounted
+  state = {
+    currentUser: null
+  }
+
+  render(){
+    if(this.state.currentUser !== null){
+      return (
+        <div className="App">
+          <LayoutComponent/>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div className="App">
+          <LoginComponent/>
+        </div>
+      );
+    }
+
+  }
+
 }
 
 export default App;
---
+++ src/components/LoginComponent.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Form, Input, Button,Checkbox , Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+
+class LoginComponent extends Component {
+
+    state = {
+        email: '',
+        password: '',
+    }
+
+
+    onFinish = (e) => {
+        // here will go call to authorization service. On 200 token will be stored in localStorage
+        // After success user will be redirected to HomePage
+        console.log(`Submitted data: email ${this.state.email} password ${this.state.password}`);
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+        <Row style={{'margin-top': '15%'}}>
+            <Col span={8}></Col>
+                <Col span={8}>
+                    <Form
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={this.onFinish}
+                    >
+                        <Form.Item onChange ={this.handleChange}
+                            name="email"
+                            rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+                        </Form.Item>
+                        <Form.Item onChange ={this.handleChange}
+                            name="password"
+                            rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                            ]}
+                        >
+                            <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <a className="login-form-forgot" href="">
+                            Forgot password
+                            </a>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                            </Button>
+                            Or <a href="">register now!</a>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            <Col span={8}></Col>
+          </Row>
+        )
+    }
+}
+export default LoginComponent;
