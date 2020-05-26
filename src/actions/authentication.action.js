994b407efbc3169ad77af5bf542dd0172a1ce869
Grzegorz Andruszkiewicz <grzegorz.andruszkiewicz@softwarehut.com>
2020-05-26 17:40:51 +0200

    redux implemented to registration component

--- src/actions/authentication.action.js
+++ src/actions/authentication.action.js
@@ -5,13 +5,35 @@ import history from '../helpers/history';
 
 
 export const auhenticationActions = {
-    login
+    login,
+    register
 }
 
 function login(email, password){
     return dispatch => {
         dispatch(request({email}));
         authenticationService.login(email, password)
+            .then(
+                user => {
+                    dispatch(success(user));
+                    history.push('/');
+                },
+                error => {
+                    dispatch(failure(error));
+                    dispatch(alertActions.error(error));
+                }
+            )
+    };
+
+    function request(user) { return { type: authenticationConstants.LOGIN_REQUEST, user } }
+    function success(user) { return { type: authenticationConstants.LOGIN_SUCCESS, user } }
+    function failure(error) { return { type: authenticationConstants.LOGIN_FAILURE, error } }
+}
+
+function register(email, password){
+    return dispatch => {
+        dispatch(request({email}));
+        authenticationService.register(email, password)
             .then(
                 user => {
                     dispatch(success(user));
--- src/components/Authentication/RegistrationComponent.js
+++ src/components/Authentication/RegistrationComponent.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {authenticationService} from '../../services/authentication.service';
 import { AuthenticationForm } from './AuthenticationForm';
 import { useHistory  } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { auhenticationActions } from '../../actions/authentication.action'
 
 
 
-const RegistrationComponent = () => {
+class RegistrationComponent extends Component{
+    constructor(props){
+        super(props);
 
-    let history = useHistory();
-
-    const onFinish = (e) => {
-        authenticationService.register(e.email, e.password)
-            .then(result =>{
-                if(result){
-                    history.push("/");
-                }
-            });
+        this.handleFinish = this.handleFinish.bind(this);
     }
 
-    return(
-        <AuthenticationForm 
-            onFinish ={onFinish}
-            submitButtonValue = {"Register"}
-            displayRegister = {"hidden"}
-            loginSection = {"none"}
-        />
-    )
-    
+    handleFinish = (e) => {
+        const { dispatch } = this.props;
+        dispatch(auhenticationActions.register(e.email, e.password));
+    }
+
+    render(){
+        return(
+            <AuthenticationForm 
+                onFinish ={this.handleFinish}
+                submitButtonValue = {"Register"}
+                displayRegister = {"hidden"}
+                loginSection = {"none"}
+            />
+        )
+    }
 }
-export default RegistrationComponent;
+
+const mapStateToProps = (state) =>{
+    return{
+        state:state
+    }
+}
+
+export default connect(mapStateToProps)(RegistrationComponent);
